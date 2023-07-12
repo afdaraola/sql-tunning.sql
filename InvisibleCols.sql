21529f16e65b6fcc67396f340aea4eba806817bc
Festus Daramola <123788749+afdaraola@users.noreply.github.com>
2023-07-12 07:47:57 -0400

    Create InvisibleCols.sql

---
+++ InvisibleCols.sql
@@ -0,0 +1,12 @@
+/*
+
+Some miscellaneous facts about invisible columns include the following.
+
+Virtual columns can be made invisible.
+A table can be partitioned by an invisible column, either during or after table creation.
+External, cluster and temporary tables can not have invisible columns.
+User-defined types can not contain invisible attributes.
+You can not make system generated hidden columns visible.
+
+
+*/
